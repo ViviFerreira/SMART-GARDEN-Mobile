771d8d033af9a8ace4d7d628415a66b2d2a47c2b
Mateus8741 <mateus_sousa_tavares@hotmail.com>
2022-05-31 21:26:31 -0300

    Fix FlatList rederind

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Main } from './styles';
-import { StatusBar } from 'expo-status-bar';
-import Header from './Header';
-import { colorStatusBar } from '../UI/variaveis';
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Main } from "./styles";
+import { StatusBar } from "expo-status-bar";
+import Header from "./Header";
+import { colorStatusBar } from "../UI/variaveis";
 
 export default function Layout({ children }) {
   return (
     <SafeAreaView>
-      <StatusBar backgroundColor={colorStatusBar} />
+      <StatusBar backgroundColor={colorStatusBar} style="auto" />
       <Header />
       <Main>{children}</Main>
     </SafeAreaView>
